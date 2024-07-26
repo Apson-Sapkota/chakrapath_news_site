2da5c4e6db5d03bd474126f6ce28457721fb0f04
Apson-Sapkota <tiger.white10a@gmail.com>
2024-07-26 20:50:12 +0545

    Initializing trending

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from './Header'
 import News1 from './News1'
 import News2 from './News2'
+import Trending from './Trending'
 
 function App() {
   return (
     <>
-    <Header/>
-    <News1/>
-    <News2/>
+    {/* <Header/>
+    <News1/> */}
+    {/* <News2/> */}
+    <Trending/>
     </>
   )
 }
